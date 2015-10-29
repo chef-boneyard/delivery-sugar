1b2af39243cb4abcaf453241fe9cfcc3c9b1b1f1
Salim Afiune <afiune@chef.io>
2015-10-29 08:07:50 -0700

    Use test-kitchen in Unit instead of Quality

--- .delivery/build_cookbook/recipes/unit.rb
+++ .delivery/build_cookbook/recipes/unit.rb
@@ -3,3 +3,10 @@ execute 'rspec --format documentation --color' do
   cwd node['delivery']['workspace']['repo']
   environment("COVERAGE" => 'true')
 end
+
+# Execute a test-kitchen converge and destroy in EC2
+delivery_test_kitchen 'quality' do
+  yaml '.kitchen.ec2.yml'
+  driver 'ec2'
+  action [:converge, :destroy]
+end
