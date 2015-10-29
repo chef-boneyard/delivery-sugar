fb9bc2f390746abceb006023fbc96f0b8ac6c083
Salim Afiune <afiune@chef.io>
2015-10-29 00:38:54 -0700

    Run test-kitchen in quality

--- .delivery/build_cookbook/recipes/quality.rb
+++ .delivery/build_cookbook/recipes/quality.rb
@@ -1,1 +1,6 @@
-# Skip Phase
+# Execute a test-kitchen converge and destroy in EC2
+delivery_test_kitchen 'quality' do
+  yaml '.kitchen.ec2.yml'
+  driver 'ec2'
+  action [:converge, :destroy]
+end
