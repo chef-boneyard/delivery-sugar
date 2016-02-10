7a0f1b5902737f0dac086f8e25cc19cae04115a1
Tyler Cloke <tylercloke@gmail.com>
2016-02-10 15:37:28 -0800

    Appease the rubocop gods.

--- spec/support/example_knife.rb
+++ spec/support/example_knife.rb
@@ -1,7 +1,7 @@
 current_dir = File.dirname(__FILE__)
-log_location      STDOUT
-node_name         'delivery'
-client_key        "#{current_dir}/delivery.pem"
+log_location STDOUT
+node_name 'delivery'
+client_key "#{current_dir}/delivery.pem"
 trusted_certs_dir '/etc/chef/trusted_certs'
-chef_server_url   'https://172.31.6.129/organizations/chef_delivery'
+chef_server_url 'https://172.31.6.129/organizations/chef_delivery'
 encrypted_data_bag_secret '/var/opt/delivery/etc/secret'
