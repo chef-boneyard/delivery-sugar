491f363720a36842a6e156698eb83e69cfb92711
Salim Afiune <afiune@chef.io>
2016-07-21 14:34:24 -0400

    Consume new delivery_supmermarket LWRP in deploy

--- .delivery/build_cookbook/recipes/deploy.rb
+++ .delivery/build_cookbook/recipes/deploy.rb
@@ -18,29 +18,16 @@ if delivery_environment == 'delivered'
   end
 
   # Release to Supermarket
-  supermarket_site = "https://supermarket.chef.io"
-  cookbook = DeliverySugar::Cookbook.new(node['delivery']['workspace']['repo'])
-
   if secrets['supermarket_user'].nil? || secrets['supermarket_user'].empty?
-    Chef::Log.fatal "If supermarket-custom-credentials is set to true, you must add supermarket_user to the secrets data bag."
-    raise RuntimeError, "supermarket-custom-credentials was true and supermarket_user was not defined in delivery secrets."
+    raise RuntimeError, "supermarket_user was not defined in delivery secrets."
   end
 
   if secrets['supermarket_key'].nil? || secrets['supermarket_key'].nil?
-    Chef::Log.fatal "If supermarket-custom-credentials is set to true, you must add supermarket_key to the secrets data bag."
-    raise RuntimeError, "supermarket-custom-credentials was true and supermarket_key was not defined in delivery secrets."
+    raise RuntimeError, "supermarket_key was not defined in delivery secrets."
   end
 
-  execute "share_cookbook_to_supermarket_#{cookbook.name}" do
-    command "echo '#{secrets['supermarket_key']}' | knife supermarket " \
-            "share #{cookbook.name} " \
-            "--cookbook-path #{cookbook.path} " \
-            "--config #{delivery_knife_rb} " \
-            "--supermarket-site #{supermarket_site} " \
-            "-u #{secrets['supermarket_user']} " \
-            "-k /dev/stdin"
-    not_if "knife supermarket show #{cookbook.name} #{cookbook.version} " \
-            "--config #{delivery_knife_rb} " \
-            "--supermarket-site #{supermarket_site}"
+  delivery_supermarket "share_cookbook_to_supermarket" do
+    user secrets['supermarket_user']
+    key secrets['supermarket_key']
   end
 end
