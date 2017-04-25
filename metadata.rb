587c49f31bfc0a5d85b8d6700f67ae5bda95d9a9
Tim Smith <tsmith@chef.io>
2017-04-24 23:19:26 -0700

    Use a SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'delivery-sugar'
 maintainer 'Chef Delivery Team'
 maintainer_email 'delivery-team@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Syntatic sugars for Delivery build cookbooks'
 
 version '1.3.2'
