de88194e650d824f1d17ffb287d75178ec097c82
Salim Afiune <afiune@chef.io>
2017-04-25 16:45:14 -0400

    Unpin chef-vault since 3.0.1 has been released
    Closes https://github.com/chef-cookbooks/delivery-sugar/issues/30
    
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'delivery-team@chef.io'
 license 'Apache 2.0'
 description 'Syntatic sugars for Delivery build cookbooks'
 
-version '1.4.0'
+version '1.4.1'
 
-gem 'chef-vault', '~> 3.0.1'
+gem 'chef-vault'
 
 source_url 'https://github.com/chef-cookbooks/delivery-sugar'
 issues_url 'https://github.com/chef-cookbooks/delivery-sugar/issues'
