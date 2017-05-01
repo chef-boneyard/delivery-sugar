aef012d795c67766712e3409b5b122c0b95e853e
Salim Afiune <afiune@chef.io>
2017-05-01 15:30:49 -0400

    Remove gem dependency from metadata
    This change is removing the `gem 'chef-vault'`from the `metadata.rb`
    since it is breaking air-gapped environments because is trying to reach
    out to rubygems.org
    
    The right solution is to add an option to the `automate-ctl install-runner`
    command that sets up the `Chef::Config[:rubygems_url]` to the users rubygem
    internal mirror and then put back the `gem` dependency in the
    `metadata`. This will solve all the gem problems on air-gapped
    environments moving forward.
    
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,7 @@ maintainer_email 'delivery-team@chef.io'
 license 'Apache 2.0'
 description 'Syntatic sugars for Delivery build cookbooks'
 
-version '1.4.1'
-
-gem 'chef-vault'
+version '1.5.0'
 
 source_url 'https://github.com/chef-cookbooks/delivery-sugar'
 issues_url 'https://github.com/chef-cookbooks/delivery-sugar/issues'
